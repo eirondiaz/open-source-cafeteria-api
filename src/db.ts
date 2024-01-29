0aa2b304a05abf0510786a2883580c082980de39
eirondiaz <eiron.diaz-ext@ab-inbev.com>
2024-01-29 15:12:22 -0400

    fix

--- src/db.ts
+++ src/db.ts
@@ -7,7 +7,7 @@ dotenv.config()
 export const AppDataSource = new DataSource({
   type: 'mssql',
   host: process.env.DB_HOST || '',
-  port: Number(process.env.PORT) || 0,
+  port: Number(process.env.DB_PORT) || 0,
   username: process.env.DB_USER || '',
   password: process.env.DB_PASS || '',
   database: process.env.NAME || '',
