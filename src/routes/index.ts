448c784b15361907447d0b019fe9d7377355aa3d
eirondiaz <eiron.diaz-ext@ab-inbev.com>
2024-02-05 13:34:47 -0400

    changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import CampusRouter from './campus.routes'
 import UserRouter from './user.routes'
 import SupplierRouter from './supplier.routes'
+import UserTypeRouter from './user-type.routes'
 
 const router = Router()
 
 router.use('/campus', CampusRouter)
 router.use('/users', UserRouter)
 router.use('/suppliers', SupplierRouter)
+router.use('/user-types', UserTypeRouter)
 
 export default router
