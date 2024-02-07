98abb780f186566a6bc59c4be4c81e51d6869eaf
eirondiaz <eiron.diaz-ext@ab-inbev.com>
2024-02-06 23:01:02 -0400

    ..

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,11 @@ import CampusRouter from './campus.routes'
 import UserRouter from './user.routes'
 import SupplierRouter from './supplier.routes'
 import UserTypeRouter from './user-type.routes'
+import CafeteriaRouter from './cafeteria.routes'
 
 const router = Router()
 
+router.use('/cafeterias', CafeteriaRouter)
 router.use('/campus', CampusRouter)
 router.use('/users', UserRouter)
 router.use('/suppliers', SupplierRouter)
